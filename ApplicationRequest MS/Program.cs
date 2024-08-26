23974c80cc28f9af70a3bbff01c97cd791b70cd4
SasiKudasi <sasha.mittal@yandex.ru>
2024-08-26 05:45:22 +0300

    ToDo in Main

--- ApplicationRequest MS/Program.cs
+++ ApplicationRequest MS/Program.cs
@@ -4,6 +4,14 @@
     {
         static void Main(string[] args)
         {
+            /*
+             * ToDo
+             * Подключить ConnectionStrings
+             * Реализовать подлючение RabbitMQ
+             * Обработать валидацию данных
+             * Подумать о том как возвращать какие то данные в кор приложение
+             */
+
             Console.WriteLine("Hello, World!");
         }
     }
